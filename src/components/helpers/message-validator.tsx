e01f92389dd476d36110c6ba65861b7621ff6401
Maakep <schon.fredrik@gmail.com>
2017-06-17 12:57:08 +0200

    Inventory listing

--- src/components/helpers/message-validator.tsx
+++ src/components/helpers/message-validator.tsx
@@ -1,8 +1,11 @@
 import Player from '../Player';
 import {Location, Item} from './types';
+import * as _String from './StringHelper';
+import * as TextFormat from './TextFormat';
 
 const WALK = {
     CMD: 'walk',
+    DESC: 'Navigate north, south, east or west',
     NORTH: 'north',
     SOUTH: 'south',
     WEST: 'west',
@@ -11,8 +14,18 @@ const WALK = {
 
 const SLASH = {
     CMD: '/',
-    HELP: '/help',
-    STATS: '/stats',
+    HELP: {
+        CMD: '/help',
+        DESC: 'Display available commands'
+    },
+    STATS: {
+        CMD: '/stats',
+        DESC: 'Display your statistics'
+    },
+    INVENTORY: {
+        CMD: '/inventory',
+        DESC: 'Display your inventory'
+    },
 }
 
 const TAKE = {
@@ -41,15 +54,24 @@ export function validateMessage(obj: Player, msg: string, socket: SocketIOClient
             default:
                 splitMsg[1] == null
         }
-        obj.addMessage('You venture ' + ((splitMsg[1] != null) ? splitMsg[1] : 'around in circles, getting nowhere'), );
+        obj.addMessage('You venture ' + ((!_String.isNullOrWhitespace(splitMsg[1])) ? splitMsg[1] : 'around in circles, getting nowhere'), );
         socket.emit('client:move', {from: obj.state.location.coordinates, to: coord});
-    } else if (splitMsg[0][0] === SLASH.CMD) {
-        if(splitMsg[0] === SLASH.HELP) {
-            obj.addMessage('<walk [(north|south|west|east)]>');
-            obj.addMessage('/help - This text');
-            obj.addMessage('/stats - Your stats');
-        } else if (splitMsg[0] === SLASH.STATS) {
+    } else if (splitMsg[0][0] === SLASH.CMD) { // First letter
+        if(splitMsg[0] === SLASH.HELP.CMD) {
+            obj.addMessage(SLASH.HELP.CMD + ' - ' + SLASH.HELP.DESC);
+            obj.addMessage(WALK.CMD + ' - ' + WALK.DESC);
+            obj.addMessage(SLASH.STATS.CMD + ' - ' + SLASH.STATS.DESC);
+            obj.addMessage(SLASH.INVENTORY.CMD + ' - ' + SLASH.INVENTORY.DESC);
+        } else if (splitMsg[0] === SLASH.STATS.CMD) {
             obj.addMessage('You are currently at {' + obj.state.location.coordinates.x + ', ' + obj.state.location.coordinates.y + '}');
+        } else if (splitMsg [0] === SLASH.INVENTORY.CMD) {
+            let inventory: Item[] = obj.state.inventory;
+            let items: string = TextFormat.commaSeparatedArray(inventory, 'name');
+            if (items != null) {
+                obj.addMessage(items);
+            } else {
+                obj.addMessage('No items in inventory');
+            }
         }
         return false;
     } else if (splitMsg[0] === TAKE.CMD) {
